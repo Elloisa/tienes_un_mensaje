5b0c6d62cdb99b6cc3e81f6ce5348bc996dd724c
Lorena Tito Ramos <lorena.tito.ramos@gmail.com>
2023-04-27 00:12:53 -0500

    Automatizar envìo de correo
    my_bat.bat para cargar ruta de ejecuciòn de archivo
    leer_correo.py se guardò contraseña de aplicativo dentro de variable

--- leer_csv_file.py
+++ leer_csv_file.py
@@ -1,5 +1,8 @@
 import pandas
 df = pandas.read_csv('lista_correos.csv', delimiter = ';', index_col = "Nombre")
+print(df)
 
 for i in range(len(df)):
     print(df.iloc[i]['email'])
+
+    
--- send_email.py
+++ send_email.py
@@ -8,28 +8,35 @@ Created on Monday April 24 23:37:00 2023
 #STARTING A SECURE SMTP CONNECTION==============================================================================
 
 import smtplib, ssl
+import pandas
 
 smtp_server = "smtp.gmail.com"
 port = 587  # For starttls
 sender_email = "unnilorena@gmail.com"
-receiver_email = "lorena.tito.ramos@gmail.com"
-password = input("Type your password and press enter: ")
-message = """\
-Subject: Hi there
+password = "mmme aahd lexu ruhw"
 
-This message is sent from Python."""
+#Read CSV file that contains reciever emails
+
 
 # Create a secure SSL context
 context = ssl.create_default_context()
 
-
 # Try to log in to server and send email
 with smtplib.SMTP(smtp_server, port) as server:
+    
     server.starttls(context=context)
     server.login(sender_email, password)
 
+    df = pandas.read_csv('lista_correos.csv', delimiter = ';', index_col = "Nombre")
+
     try:
-        server.sendmail(sender_email, receiver_email, message)
+        for i in range(len(df)):
+            receiver_email = df.iloc[i]['email']
+            message = f"""\
+            Subject: Hi there
+
+            {df.iloc[i]['Mensaje']}"""
+            server.sendmail(sender_email, receiver_email, message)
 
     except Exception as e:
         # Print any error messages to stdout
